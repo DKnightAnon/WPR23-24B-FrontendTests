f7041f0ec198e169ece44ae0eadd594e74fc5460
DKnightAnon <a.r.c.delgado@outlook.com>
2024-01-21 20:48:35 +0100

    Tests voor login - chat aangepast.

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,9 +10,11 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+
+  timeout : 100000,
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -20,24 +20,26 @@ test('login as user - invalid sign in', async ({ page }) => {
 });
 
 test('login as user - valid sign in', async ({ page }) => {
+    // await page.goto('https://wpr23-24b.azurewebsites.net/');
+    // await page.locator('h1').click();
+    // await page.getByRole('link', { name: 'Inloggen' }).click();
+    // await page.getByPlaceholder('Emailadres').click();
+    // await page.getByPlaceholder('Emailadres').fill('user@example.com');
+    // await page.getByPlaceholder('Wachtwoord').click();
+    // await page.getByPlaceholder('Wachtwoord').fill('SterkWachtwoord@2');
+    // await page.getByLabel('Login Knop').click();
+
     await page.goto('https://wpr23-24b.azurewebsites.net/');
-    await page.locator('h1').click();
     await page.getByRole('link', { name: 'Inloggen' }).click();
     await page.getByPlaceholder('Emailadres').click();
     await page.getByPlaceholder('Emailadres').fill('user@example.com');
-    await page.getByPlaceholder('Wachtwoord').click();
+    await page.getByPlaceholder('Emailadres').press('Tab');
     await page.getByPlaceholder('Wachtwoord').fill('SterkWachtwoord@2');
-    await page.getByLabel('Login Knop').click();
-    await page.getByText('Gebruiker GegevensOnderzoeken').click();
-    await page.getByRole('heading', { name: 'Gebruiker Gegevens' }).click();
-    await page.getByText('Mijn GegevensVolledige').click();
-    await page.getByText('Volledige Naam').click();
-    await page.getByText('Emailadres:').click();
-    await page.getByText('Telefoonnummer:', { exact: true }).click();
-    await page.getByText('Postcode:').click();
-    await page.getByText('Geboortedatum:').click();
-    await page.getByRole('heading', { name: 'Mijn Communicatievoorkeur' }).click();
-    await page.getByRole('heading', { name: 'Mijn Communicatievoorkeur' }).click();
+    await page.getByPlaceholder('Wachtwoord').press('Tab');
+    await page.getByLabel('Login', { exact: true }).press('Tab');
+    await page.getByLabel('Login Knop').press('Enter');
+    await page.getByRole('heading', { name: 'Welkom bij het' }).click();
+    await page.getByText('Uw centrale hub voor').click();
 
 
 
@@ -47,37 +49,41 @@ test('login as company - invalid sign in', async ({ page }) => { });
 
 test('login as user - navigate to chat', async ({ page }) => {
 
-    //Klikken op berichten gaat fout. Misschien omdat het een div in een list element is?
-    await page.goto('https://wpr23-24b.azurewebsites.net/');
-    await page.locator('section').filter({ hasText: 'Stichting AccessibilitySamen' }).click({ button: 'right' });
-    await page.getByRole('link', { name: 'Inloggen' }).click();
-    await page.getByPlaceholder('Emailadres').click();
-    await page.getByPlaceholder('Emailadres').fill('user@example.com');
-    await page.getByPlaceholder('Emailadres').press('Tab');
-    await page.getByPlaceholder('Wachtwoord').fill('SterkWachtwoord@2');
-    await page.getByLabel('Login Knop').click();
-    await page.getByRole('heading', { name: 'Gebruiker Gegevens' }).click();
-    await page.getByRole('link', { name: 'Chat' }).click();
-    // await page.getByText('System - 1/19/2024, 3:02:29 PM Deze chatgroep heeft nog geen berichten!').click();
-    await page.getByLabel('Huidige verbonden gesprek').click();
-    await page.getByPlaceholder('Typ een bericht...').click();
-    await page.getByRole('button', { name: 'Submit' }).click();
-    await page.getByRole('button', { name: 'Testgesprek 15/1/' }).click();
-    await page.getByPlaceholder('Typ een bericht...').click();
-    await page.getByPlaceholder('Typ een bericht...').fill('Playwright Testbericht');
-    await page.getByRole('button', { name: 'Submit' }).click();
-    // await page.getByText('user@example.com - 1/19/2024, 3:03:26 PM Playwright Testbericht').click();
-    await page.getByText('user@example.com - 1/19/2024').click();
-    await page.getByText('Playwright Testbericht').click();
-    await page.locator('div').filter({ hasText: 'Gespreklijst Nieuw gesprek' }).nth(2).click();
-    await page.getByRole('button', { name: 'nieuwfillertest' }).click();
-    // await page.getByText('System - 1/19/2024, 3:03:48 PM Deze chatgroep heeft nog geen berichten!').click();
+
+await page.goto('https://wpr23-24b.azurewebsites.net/login');
+await page.getByPlaceholder('Emailadres').click();
+await page.getByPlaceholder('Emailadres').fill('user@example.com');
+await page.getByPlaceholder('Emailadres').press('Tab');
+await page.getByPlaceholder('Wachtwoord').click();
+await page.getByPlaceholder('Wachtwoord').press('Shift+Tab');
+await page.getByPlaceholder('Emailadres').press('Tab');
+await page.getByPlaceholder('Wachtwoord').fill('SterkWachtwoord@2');
+await page.getByLabel('Login Knop').click();
+await page.getByRole('heading', { name: 'Welkom bij het' }).click();
+await page.getByRole('link', { name: 'Chat' }).click();
+await page.getByLabel('Huidige verbonden gesprek').click();
+// await page.getByText('System - 1/21/2024, 2:47:04 PM Deze chatgroep heeft nog geen berichten!').click();
 });
 
-test('no login - check if private routing elements exists', async ({ page }) => { 
+test('no login - check if private routing elements exists - should not exist ', async ({ page }) => { 
     await page.goto('https://wpr23-24b.azurewebsites.net/');
     await page.getByRole('banner').click();
     await page.getByText('HomeAboutServicesNewsContactInloggen').click();
     await expect(page.getByRole('link', {name:'Chat'})).toHaveCount(0);
     await expect(page.getByRole('link', {name:'Beheer'})).toHaveCount(0);
 });
+
+test('no login - check if private routing elements exists - should exist ', async ({ page }) => { 
+    await page.goto('https://wpr23-24b.azurewebsites.net/');
+await page.getByRole('link', { name: 'Inloggen' }).click();
+
+await page.getByPlaceholder('Emailadres').click();
+await page.getByPlaceholder('Emailadres').fill('user@example3.com');
+await page.getByPlaceholder('Wachtwoord').click();
+await page.getByPlaceholder('Wachtwoord').click();
+await page.getByPlaceholder('Wachtwoord').fill('SterkWachtwoord@2');
+
+    // await expect(page.getByRole('link', {name:'Chat'})).toHaveText("Chat");
+    // await expect(page.getByRole('link', {name:'Beheer'})).toHaveCount(1);
+});
+
